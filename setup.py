3b11b3b71cc55b3a1d6f6b4dd743ad723264f941
Lyricorpse <lyricorpse@gmail.com>
2014-12-31 12:18:25 -0600

    Add setup.py
    Now one can `import pyesg` anywhere after running `python3 setup.py
    install`

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name = 'pyesg',
+    py_modules = ['pyesg'],
+    version = '0.0.1',
+    description = 'Python library of Earth Spherical Geometry (PyESG)',
+    author='Feng Zhu',
+    author_email='feng.zhu@ssec.wisc.edu',
+    url = 'https://github.com/lyricorpse/PyESG',
+    license='ISC',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python :: 3'
+    ]
+)
+
